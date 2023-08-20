64383884ac27b2f0e36e2ccc28f00031c9952fc5
Aditya Bansal <bansaladitya@gmail.com>
2023-08-20 19:59:32 +0530

    comleted the home page more or less

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home-page/home/home.component';
-
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+
 ];
 
 @NgModule({
