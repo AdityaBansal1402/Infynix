ffef91c6e82d93015fcec9ae6437b7b75b215185
rchievj <archievijay02@gmail.com>
2023-08-21 20:03:01 +0530

    kaam ho gaya

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home-page/home/home.component';
+import  {MainPageComponent} from './security/main-page/main-page.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path:'security', component: MainPageComponent}
 ];
 
 @NgModule({
