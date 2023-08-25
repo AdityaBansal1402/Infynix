b2ed88bac50fbd3fce6612f3ef62f5fa97b87dbe
Nimbuwalla <nimbuwalla@gmail.com>
2023-08-25 17:20:58 +0530

    lets goooo

---
+++ src/app/careers/careers.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CareersComponent } from './careers/careers.component';
+
+
+
+@NgModule({
+  declarations: [
+    CareersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CareersModule { }
